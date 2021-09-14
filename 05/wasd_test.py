950795cd7b59da64da808ca449546afe832b1d33
niche0905 <yudle537@gmail.com>
2021-09-14 19:16:03 +0900

    drill 05 완성
    https://youtu.be/ty0NEQxQQlA

---
+++ 05/wasd_test.py
@@ -0,0 +1,33 @@
+import turtle
+
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+    
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+
+def restart():
+    turtle.reset()
+
+turtle.shape('turtle')
+
+turtle.onkey(right, 'd')
+turtle.onkey(up, 'w')
+turtle.onkey(left, 'a')
+turtle.onkey(down, 's')
+turtle.onkey(restart, 'Escape')
+turtle.listen()
